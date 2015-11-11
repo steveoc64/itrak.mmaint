34f87b57bd91380602d4b3ac0c0638a54a28607f
steveoc64 <steveoc64@gmail.com>
2015-11-11 13:09:28 +1030

    Add some code to import and sanitize database

---
+++ SQL/fixdata.go
@@ -0,0 +1,265 @@
+// Quick and Dirty program to create all the relations in the (new) database
+
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var db *sql.DB
+
+// Runtime variables, held in external file config.json
+type iTrakMMaintConfig struct {
+	Debug          bool
+	DataSourceName string
+	WebPort        int
+}
+
+var itrak iTrakMMaintConfig
+
+// Load the config.json file, and override with runtime flags
+func LoadConfig() {
+	cf, err := os.Open("../config.json")
+	if err != nil {
+		log.Println("Could not open ../config.json :", err.Error())
+	}
+
+	data := json.NewDecoder(cf)
+	if err = data.Decode(&itrak); err != nil {
+		log.Fatalln("Failed to load config.json :", err.Error())
+	}
+
+	log.Printf("Starting\n\tDebug: \t\t%t\n\tSQLServer: \t%s\n\tWeb Port: \t%d\n",
+		itrak.Debug,
+		itrak.DataSourceName,
+		itrak.WebPort)
+}
+
+func SQLMap(db *sql.DB, sqlStatement string, sqlParams ...interface{}) ([]map[string]string, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	var results []map[string]string
+	rows, err := db.Query(sqlStatement, sqlParams...)
+	if err != nil {
+		fmt.Println("Error executing: ", sqlStatement)
+		return results, err
+	}
+	cols, _ := rows.Columns()
+	colsLower := make([]string, len(cols))
+
+	for i, v := range cols {
+		colsLower[i] = strings.ToLower(v)
+	}
+
+	rawResult := make([][]byte, len(cols))
+
+	dest := make([]interface{}, len(cols)) // A temporary interface{} slice
+	for i, _ := range rawResult {
+		dest[i] = &rawResult[i] // Put pointers to each string in the interface slice
+	}
+
+	for rows.Next() {
+		result := make(map[string]string, len(cols))
+		rows.Scan(dest...)
+		for i, raw := range rawResult {
+			if raw == nil {
+				result[cols[i]] = ""
+			} else {
+				result[cols[i]] = string(raw)
+			}
+		}
+		results = append(results, result)
+	}
+	return results, nil
+}
+
+func ExecDb(db *sql.DB, sqlStatement string, sqlParams ...interface{}) (int64, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	result, err := db.Exec(sqlStatement, sqlParams...)
+	if err != nil {
+		fmt.Println("Error executing: ", sqlStatement)
+		fmt.Println(err)
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+func main() {
+
+	// Connect to the SQLServer
+	var err error
+
+	LoadConfig()
+
+	db, err = sql.Open("postgres", itrak.DataSourceName)
+	defer db.Close()
+	if err != nil {
+		log.Fatalln("Exiting ..", err.Error())
+	}
+	log.Println("DSN:", itrak.DataSourceName)
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	// Person table
+	//   - fix location
+	sqlResult, _err := SQLMap(db, "select * from fm_person")
+	if _err != nil {
+		log.Println("Error:", _err.Error())
+	}
+	for _, row := range sqlResult {
+		// Lookup the location id
+		_res, _err := SQLMap(db, "select id from site where name=$1", row["location"])
+		if _err != nil {
+			log.Fatal(_err.Error())
+		} else {
+			siteId := _res[0]["id"]
+			personName := row["personname"]
+			updatedRows, __err := ExecDb(db, "update person set location=$2 where name=$1",
+				personName,
+				siteId)
+
+			if __err != nil {
+				log.Fatal(__err.Error())
+			} else {
+				log.Println("Updated", updatedRows, "persons setting location =", siteId, "where name =", personName)
+			}
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	// Equipment table
+	//		- fix location
+	//		- fix the category
+	// 		- fix the vendor
+	//		- fix the parent ID
+	sqlResult, _err = SQLMap(db, "select * from fm_equipment")
+	if _err != nil {
+		log.Println("Error:", _err.Error())
+	}
+	for _, row := range sqlResult {
+		// Lookup the location id
+		_res, _err := SQLMap(db, "select id from site where name=$1", row["location"])
+		if _err != nil {
+			log.Fatal(_err.Error())
+		}
+		var siteId int
+		if len(_res) > 0 {
+			siteId, _err = strconv.Atoi(_res[0]["id"])
+		}
+
+		// now get the category ID
+		var catId int
+		_res, _err = SQLMap(db, "select id from equip_type where name=$1", row["category"])
+		if _err != nil {
+			log.Fatal(_err.Error())
+		}
+		if len(_res) > 0 {
+			catId, _err = strconv.Atoi(_res[0]["id"])
+		}
+
+		// get the parentId
+		var parentId int
+		if row["partof"] != "" {
+			_res, _err = SQLMap(db, "select id from equipment where name=$1", row["partof"])
+			if _err != nil {
+				log.Fatal(err.Error())
+			}
+			if len(_res) > 0 {
+				parentId, _err = strconv.Atoi(_res[0]["id"])
+			}
+		}
+
+		// Now get the Vendor
+		vendorId := 0
+
+		updatedRows, _ := ExecDb(db, "update equipment set location=$2,category=$3,vendor=$4,parent_id=$5 where name=$1",
+			row["name"],
+			siteId,
+			catId,
+			vendorId,
+			parentId)
+		ExecDb(db, "update equipment set location=null where location=0")
+		ExecDb(db, "update equipment set category=null where category=0")
+		ExecDb(db, "update equipment set vendor=null where vendor=0")
+		ExecDb(db, "update equipment set parent_id=null where parent_id=0")
+		log.Println("Updated", updatedRows, "equipments setting location =", siteId,
+			", category =", catId,
+			", vendor =", vendorId,
+			", parent part =", parentId,
+			"where name =", row["name"])
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	// Vendor table
+	//		- fix rating
+	sqlResult, _err = SQLMap(db, "select * from fm_equipment")
+	if _err != nil {
+		log.Println("Error:", _err.Error())
+	}
+	for _, row := range sqlResult {
+		// Lookup the location id
+		_res, _err := SQLMap(db, "select id from site where name=$1", row["location"])
+		if _err != nil {
+			log.Fatal(_err.Error())
+		}
+		var siteId int
+		if len(_res) > 0 {
+			siteId, _err = strconv.Atoi(_res[0]["id"])
+		}
+
+		// now get the category ID
+		var catId int
+		_res, _err = SQLMap(db, "select id from equip_type where name=$1", row["category"])
+		if _err != nil {
+			log.Fatal(_err.Error())
+		}
+		if len(_res) > 0 {
+			catId, _err = strconv.Atoi(_res[0]["id"])
+		}
+
+		// get the parentId
+		var parentId int
+		if row["partof"] != "" {
+			_res, _err = SQLMap(db, "select id from equipment where name=$1", row["partof"])
+			if _err != nil {
+				log.Fatal(err.Error())
+			}
+			if len(_res) > 0 {
+				parentId, _err = strconv.Atoi(_res[0]["id"])
+			}
+		}
+
+		// Now get the Vendor
+		vendorId := 0
+
+		updatedRows, _ := ExecDb(db, "update equipment set location=$2,category=$3,vendor=$4,parent_id=$5 where name=$1",
+			row["name"],
+			siteId,
+			catId,
+			vendorId,
+			parentId)
+		ExecDb(db, "update equipment set location=null where location=0")
+		ExecDb(db, "update equipment set category=null where category=0")
+		ExecDb(db, "update equipment set vendor=null where vendor=0")
+		ExecDb(db, "update equipment set parent_id=null where parent_id=0")
+		log.Println("Updated", updatedRows, "equipments setting location =", siteId,
+			", category =", catId,
+			", vendor =", vendorId,
+			", parent part =", parentId,
+			"where name =", row["name"])
+	}
+}
--- SQL/init.sql
+++ SQL/init.sql
@@ -1,27 +1,149 @@
+drop table if exists roles;
+create table roles (
+	id integer primary key,
+	name text
+);
+insert into roles (id,name) values 
+	(1,'Staff'),
+	(2,'Site Manager'),
+	(3,'Vendor'),
+	(100,'Admin');
+select * from roles;
+
 drop table if exists users;
 create table users (
+	id serial primary key,
 	username varchar(32),
 	passwd varchar(32),
-	role varchar(16)
+	role integer
 );
 delete from users;
-insert into users (username,passwd,role) values ('worker1','abc123','Worker');
-insert into users (username,passwd,role) values ('worker2','abc123','Worker');
-insert into users (username,passwd,role) values ('worker3','abc123','Worker');
-insert into users (username,passwd,role) values ('sitemgr1','abc123','SiteMgr');
-insert into users (username,passwd,role) values ('sitemgr2','abc123','SiteMgr');
-insert into users (username,passwd,role) values ('sitemgr3','abc123','SiteMgr');
-insert into users (username,passwd,role) values ('subbie1','abc123','Vendor');
-insert into users (username,passwd,role) values ('subbie2','abc123','Vendor');
-insert into users (username,passwd,role) values ('subbie3','abc123','Vendor');
-insert into users (username,passwd,role) values ('admin','abc123','Admin');
+insert into users (username,passwd,role) values 
+	('worker1','abc123',1),
+	('worker2','abc123',1),
+	('worker3','abc123',1),
+	('sitemgr1','abc123',2),
+	('sitemgr2','abc123',2),
+	('sitemgr3','abc123',2),
+	('subbie1','abc123',3),
+	('subbie2','abc123',3),
+	('subbie3','abc123',3),
+	('admin','abc123',100);
+select * from users;
 
 drop table if exists sites;
-create table sites (
-	sitename varchar(32)
-	address text
-	phone text
-	contactname text
-	lat numeric(12,10)
+drop table if exists site;
+create table site (
+	id serial primary key,
+	name varchar(32),
+	address text,
+	phone text,
+	contactname text,
+	lat numeric(12,10),
 	lon numeric(12,10)
 );
+insert into site (name) values 
+	('R&D Workshop'),
+	('SBS Edinburgh'),
+	('SBS Newcastle'),
+	('SBS Minto'),
+	('SBS Tomago'),
+	('SBS Chinderah'),
+	('SBS Victoria'),
+	('Thermoloc'),
+	('Fab Shop'),
+	('USA Connecticut');
+select * from site;
+
+
+drop table if exists person;
+create table person (
+	id serial primary key,
+	user_id integer,
+	name text,
+	email text,
+	phone text,
+	hourlyrate integer,
+	comments text,
+	alternate text,
+	role integer,
+	location integer,
+	calendar integer
+);
+insert into person (name,email,phone,hourlyrate,comments,alternate,role) 
+	select personname,email,phone,hourlyrate,comments,alternate,1 
+	from fm_person;
+select * from person;
+
+drop table if exists equip_type;
+create table equip_type (
+	id serial primary key,
+	name text,
+	is_consumable boolean,
+	is_asset boolean
+);
+insert into equip_type (name,is_consumable,is_asset) values 
+	('Consumables',TRUE,FALSE),
+	('Mech',FALSE,TRUE),
+	('Spare Parts',FALSE,FALSE);
+select * from equip_type;
+
+
+drop table if exists equipment;
+create table equipment (
+	id serial primary key,
+	name text,
+	descr text,
+	comments text,
+	modelno text,
+	serialno text,
+	location integer,
+	parent_id integer,
+	category integer,
+	calendar integer,
+	vendor integer
+);
+insert into equipment (name,descr,comments,modelno,serialno)
+	select name,descr,comments,modelno,serialno
+	from fm_equipment;
+select * from equipment;
+
+drop table if exists vendor_rating;
+create table vendor_rating (
+	id serial primary key,
+	name text
+);
+insert into vendor_rating (id,name) values 
+	(1,'Good'),
+	(2,'Average'),
+	(3,'Poor');
+select * from vendor_rating;
+
+drop table if exists vendor;
+create table vendor (
+	id serial primary key,
+	name text,
+	descr text,
+	comments text,
+	account text,
+	maincontact text,
+	servicecontact text,
+	partscontact text,
+	othercontact text,
+	rating int
+);
+insert into vendor (name,descr,comments,account,maincontact,servicecontact,partscontact,othercontact)
+	select name,descr,comments,account,maincontact,servicecontact,partscontact,othercontact
+	from fm_vendor;
+select * from vendor;
+
+
+
+
+
+
+);
+
+
+
+
